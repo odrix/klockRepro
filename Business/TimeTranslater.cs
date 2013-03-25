bf619d7637dbffe76da503f7f1c759f28f6ae5b9
adriw <adribey@free.Fr>
2013-03-25 16:23:59 +0100

    gestion des minutes

--- Business/TimeTranslater.cs
+++ Business/TimeTranslater.cs
@@ -9,26 +9,55 @@ namespace klockRepro.Business
     public class TimeTranslater
     {
         string[] ChiffresHeures = new string[] { "minuit", "une", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze", "midi", "une", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze"};
-        string[] ChiffresMinute = new string[] { "", "", "", "cinq", "cinq", "cinq", "cinq", "cinq", "huit", "neuf", "dix", "onze", "midi", "une", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze" };
+        List<string> ChiffresMinute;
+
+        public TimeTranslater()
+        {
+            ChiffresMinute = new List<string>();
+            
+            for(int i=0;i<3;i++) ChiffresMinute.Add("");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("cinq");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("dix");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("et quart");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("vingt");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("vingt-cinq");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("et demi");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("moins vingt-cinq");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("moins vingt");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("moins le quart");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("moins dix");
+            for (int i = 0; i < 5; i++) ChiffresMinute.Add("moins cinq");
+            for (int i = 0; i < 2; i++) ChiffresMinute.Add("");
+        }
 
         public string Translate(DateTime time)
         {
-            string result = "";
-            string nbHourInLetter = ChiffresHeures[time.Hour];
-            string plurielHeure = time.Hour > 1 ? "s" : "";
-            if (time.Hour == 12 || time.Hour == 0)
+            StringBuilder result = new StringBuilder("il est");
+
+            if (IsSuperiorHalf(time)) time = time.AddHours(1);
+
+            result.AppendFormat(" {0}", ChiffresHeures[time.Hour]);
+            if (IsHourNeedHourLabel(time))
             {
-                result = string.Format("il est {0}", nbHourInLetter);
-            }
-            else
-            {
-                result = string.Format("il est {0} heure{1}", nbHourInLetter, plurielHeure);
+                result.Append(" heure");
+                if(time.Hour > 1) result.Append("s");
             }
 
             if(ChiffresMinute[time.Minute] != "")
-                result += " " + ChiffresMinute[time.Minute];
+                result.AppendFormat(" {0}", ChiffresMinute[time.Minute]);
 
-            return result;
+            return result.ToString();
         }
+
+        private bool IsHourNeedHourLabel(DateTime time)
+        {
+            return time.Hour != 12 && time.Hour != 0;
+        }
+
+        private bool IsSuperiorHalf(DateTime time)
+        {
+            return time.Minute > 32;
+        }
+
     }
 }
--- Testing/TestHeuresAvecMinutes.cs
+++ Testing/TestHeuresAvecMinutes.cs
@@ -18,6 +18,58 @@ namespace Testing
             TimeTranslater t = new TimeTranslater();
             string result = t.Translate(new DateTime(2013, 3, 25, 2, 5, 0));
             Assert.AreEqual("il est deux heures cinq", result);
+            result = t.Translate(new DateTime(2013, 3, 25, 2, 3, 0));
+            Assert.AreEqual("il est deux heures cinq", result);
+            result = t.Translate(new DateTime(2013, 3, 25, 2, 7, 0));
+            Assert.AreEqual("il est deux heures cinq", result);
+        }
+
+        [TestMethod]
+        public void TranslateDeuxHeureDix()
+        {
+            TimeTranslater t = new TimeTranslater();
+            string result = t.Translate(new DateTime(2013, 3, 25, 2, 10, 0));
+            Assert.AreEqual("il est deux heures dix", result);
+            result = t.Translate(new DateTime(2013, 3, 25, 2, 8, 0));
+            Assert.AreEqual("il est deux heures dix", result);
+            result = t.Translate(new DateTime(2013, 3, 25, 2, 12, 0));
+            Assert.AreEqual("il est deux heures dix", result);
+        }
+
+        [TestMethod]
+        public void TranslateDeuxHeureEtQuart()
+        {
+            TimeTranslater t = new TimeTranslater();
+            string result = t.Translate(new DateTime(2013, 3, 25, 2, 15, 0));
+            Assert.AreEqual("il est deux heures et quart", result);
+            result = t.Translate(new DateTime(2013, 3, 25, 2, 13, 0));
+            Assert.AreEqual("il est deux heures et quart", result);
+            result = t.Translate(new DateTime(2013, 3, 25, 2, 16, 0));
+            Assert.AreEqual("il est deux heures et quart", result);
+        }
+
+        [TestMethod]
+        public void TranslateTroisHeureMoinsCinq()
+        {
+            TimeTranslater t = new TimeTranslater();
+            string result = t.Translate(new DateTime(2013, 3, 25, 2, 57, 0));
+            Assert.AreEqual("il est trois heures moins cinq", result);
+            result = t.Translate(new DateTime(2013, 3, 25, 2, 57, 0));
+            Assert.AreEqual("il est trois heures moins cinq", result);
+            result = t.Translate(new DateTime(2013, 3, 25, 2, 53, 0));
+            Assert.AreEqual("il est trois heures moins cinq", result);
+        }
+
+        [TestMethod]
+        public void Translate3heures()
+        {
+            TimeTranslater t = new TimeTranslater();
+            string result = t.Translate(new DateTime(2013, 3, 25, 3, 0, 0));
+            Assert.AreEqual("il est trois heures", result);
+            result = t.Translate(new DateTime(2013, 3, 25, 2, 58, 0));
+            Assert.AreEqual("il est trois heures", result);
+            result = t.Translate(new DateTime(2013, 3, 25, 3, 2, 0));
+            Assert.AreEqual("il est trois heures", result);
         }
     }
 }
